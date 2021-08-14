1643f98d7f7abda09d90da4185684d1a0e402809
yuli_zhang <yuli_zhang@trip.com>
2021-08-14 15:35:38 +0800

    del dd

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-console.log(dd)
-dd.addEventListener('click',()=>{
-    t1()
-})
-const t1=ttt(()=>{
-    console.log(1111)
-},1000);
-function ttt(fn,delay) {
-    let flag=true;
-    return function () {
-        if(flag){
-            flag=false;
-            setTimeout(()=>{
-                fn();
-                flag=true;
-            },delay)
-        }
-    }
+console.log("self")
+// // dd.addEventListener('click',()=>{
+// //     t1()
+// // })
+// const t1 = ttt(() => {
+//     console.log(1111)
+// }, 1000);
+// function ttt(fn, delay) {
+//     let flag = true;
+//     return function () {
+//         if (flag) {
+//             flag = false;
+//             setTimeout(() => {
+//                 fn();
+//                 flag = true;
+//             }, delay)
+//         }
+//     }
 
-}
+// }
